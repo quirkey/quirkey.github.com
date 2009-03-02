24c04eb4cec140688c1eae3536ee1ae368c88a47
Aaron Quint <aaron@quirkey.com>
2009-03-02 13:59:18 -0500

    Refactoring/making more modular for jekyll

---
+++ javascripts/github.js
@@ -0,0 +1,40 @@
+var Github = {
+	initialize: function () {
+		this.fetch();
+	},
+	fetch: function() {
+		$.getJSON('http://github.com/api/v1/json/quirkey?callback=?', Quirkey.github.load);
+	},
+	load: function(github_data) {
+		var gh = Quirkey.github;
+		var repositories = github_data.user.repositories;
+		repositories.sort(function(a,b) {
+			return (a.watchers + a.forks) - (b.watchers + b.forks);
+		});
+		$('.project_holder').hide();
+		$.each(repositories, function(i, repository) {
+			if (!repository.fork && !repository.private) {
+				if (repository.watchers > 2) {
+					// popular
+					gh.displayRepository('popular', repository, true);
+				} else {
+					// other
+					gh.displayRepository('other', repository, false);
+				}
+			}
+		});
+	},
+	displayRepository: function(inside, repo, show_stats) {
+		var repo_html = '<div class="repo"><h3><a href="' + repo.url + '">' + repo.name + '</a>';
+		if (show_stats) {
+			repo_html += ' (' + repo.watchers + ')';
+		}
+		repo_html += '</h3>';
+		repo_html += '<p>' + repo.description + '</p></div>';
+		$('#projects_' + inside).prepend(repo_html);
+	}
+};
+
+$(function() {
+	Github.initialize();
+})
--- javascripts/quirkey.js
+++ javascripts/quirkey.js
@@ -1,7 +1,6 @@
 var Quirkey = {
 	initialize: function() {
 		this.quirkey_nav.initialize();
-		this.github.initialize();
 	},
 	quirkey_nav: {
 		opened: false,
@@ -31,42 +30,6 @@ var Quirkey = {
 			$('#logo').animate({'top': this.initial_top}, 400);
 			$('#quirkey_nav').slideUp(400);
 			this.opened = false;
-		}
-	},
-	github: {
-		initialize: function () {
-			this.fetch();
-		},
-		fetch: function() {
-			$.getJSON('http://github.com/api/v1/json/quirkey?callback=?', Quirkey.github.load);
-		},
-		load: function(github_data) {
-			var gh = Quirkey.github;
-			var repositories = github_data.user.repositories;
-			repositories.sort(function(a,b) {
-				return (a.watchers + a.forks) - (b.watchers + b.forks);
-			});
-			$('.project_holder').hide();
-			$.each(repositories, function(i, repository) {
-				if (!repository.fork && !repository.private) {
-					if (repository.watchers > 2) {
-						// popular
-						gh.displayRepository('popular', repository, true);
-					} else {
-						// other
-						gh.displayRepository('other', repository, false);
-					}
-				}
-			});
-		},
-		displayRepository: function(inside, repo, show_stats) {
-			var repo_html = '<div class="repo"><h3><a href="' + repo.url + '">' + repo.name + '</a>';
-			if (show_stats) {
-				repo_html += ' (' + repo.watchers + ')';
-			}
-			repo_html += '</h3>';
-			repo_html += '<p>' + repo.description + '</p></div>';
-			$('#projects_' + inside).prepend(repo_html);
 		}
 	}
 };
